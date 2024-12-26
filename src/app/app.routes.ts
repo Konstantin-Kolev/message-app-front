481624d418ed1b19fca303ef546a630bc2c0c67b
Konstantin Kolev <konstantin.kolevv@gmail.com>
2024-12-26 12:14:38 +0200

    Made routing between the auth pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ChatPageComponent } from './components/chat-page/chat-page.component';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { LoginFormComponent } from './components/authentication/login-form/login-form.component';
+import { RegisterFormComponent } from './components/authentication/register-form/register-form.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +12,14 @@ export const routes: Routes = [
   },
   {
     path: 'chat',
-    component: ChatWindowComponent
+    component: ChatPageComponent
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent
   }
 ];
--- src/app/components/chat-page/chat-page.component.ts
+++ src/app/components/chat-page/chat-page.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { ChatWindowComponent } from "../chat-window/chat-window.component";
 import { ChannelListComponent } from "../channel-list/channel-list.component";
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.model';
+import { UserStateService } from '../../services/user-state.service';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from "../../side-bar/side-bar.component";
+import { RegisterFormComponent } from "../authentication/register-form/register-form.component";
 
 @Component({
   selector: 'app-chat-page',
   standalone: true,
-  imports: [ChatWindowComponent, ChannelListComponent],
+  imports: [ChatWindowComponent, ChannelListComponent, CommonModule, SideBarComponent, RegisterFormComponent],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.css'
 })
 export class ChatPageComponent {
 
+  user$: Observable<User | null>;
+  constructor(private userStateService: UserStateService) {
+    this.user$ = this.userStateService.user$;
+  }
+
+  public loginClick() {
+    this.userStateService.setUser({
+      id: 1,
+      username: 'user1',
+      email: 'some@mail.com'
+    });
+  }
 }
