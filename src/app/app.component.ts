7515ec98774f8206c5134890ff1daf191dd75bc5
Konstantin Kolev <konstantin.kolevv@gmail.com>
2025-01-05 14:42:24 +0200

    cleaned up the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChannelListComponent } from "./components/channel-list/channel-list.component";
-import { ChatWindowComponent } from "./components/chat-window/chat-window.component";
-import { SideBarComponent } from "./side-bar/side-bar.component";
-import { Observable } from 'rxjs';
-import { User } from './models/user.model';
-import { UserStateService } from './services/user-state.service';
-import { CommonModule } from '@angular/common';
-import { LoginFormComponent } from "./components/authentication/login-form/login-form.component";
-import { RegisterFormComponent } from "./components/authentication/register-form/register-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChannelListComponent, ChatWindowComponent, SideBarComponent, CommonModule, LoginFormComponent, RegisterFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
